83f1223f6d637bb16d9a2e37f82f502e6f983be0
carriescho <carriescho@u.northwestern.edu>
2020-06-27 10:26:18 -0500

    updating files

--- ghgrp_tier_data.py
+++ ghgrp_tier_data.py
@@ -21,14 +21,17 @@ naics_county_df = pd.read_csv('naics.csv',dtype=str)
 
 #combine tier1/part75 data with capacity data by matching on facility ID, year, and unit name 
 m=tier1_df.merge(cap_df,how="inner",on=['FACILITY_ID','REPORTING_YEAR','UNIT_NAME'])
+
 m_75_cap=part75_df.merge(cap_df,how="inner",on=['FACILITY_ID','REPORTING_YEAR','UNIT_NAME'])
 
 #combine merged data with naics data by matching on facility ID
 tier1_all = m.merge(naics_county_df,how="inner",on=['FACILITY_ID'])
+
 part75_all = m_75_cap.merge(naics_county_df,how="inner",on=['FACILITY_ID'])
 
 #save to file 
 tier1_all.to_csv(r'C:\Users\Carrie Schoeneberger\Box\Boiler electrification potential\Tier1_CO2_emissions.csv', index = False)
+
 part75_all.to_csv(r'C:\Users\Carrie Schoeneberger\Box\Boiler electrification potential\Part75_CO2_emissions.csv', index = False)
 
 
